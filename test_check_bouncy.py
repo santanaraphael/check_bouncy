1be3db3be9ab3a5c3d32762cac9f28d016955cae
Raphael Santana <uroboros.phael@gmail.com>
2019-04-26 20:30:29 -0300

    Updating test case.

--- test_check_bouncy.py
+++ test_check_bouncy.py
@@ -1,16 +1,16 @@
 import unittest
-import check_bouncy
+import check_bouncy as cb
 
 
 class TestChecker(unittest.TestCase):
 
     def test_check_bouncy(self):
-        pass
+        self.assertFalse(cb.check_bouncy(134468))
+        self.assertFalse(cb.check_bouncy(66420))
+        self.assertTrue(cb.check_bouncy(155349))
+        self.assertFalse(cb.check_bouncy(0))
 
-    def test_no_cardinality(self):
-        pass
-
-    def test_mine(self):
+    def test_bouncy_number(self):
         pass
 
 
