8434b0772de55906307a398182399dee12503fb5
Raphael Santana <uroboros.phael@gmail.com>
2019-04-26 19:55:41 -0300

    Creating test file.

---
+++ test_check_bouncy.py
@@ -0,0 +1,18 @@
+import unittest
+import check_bouncy
+
+
+class TestChecker(unittest.TestCase):
+
+    def test_check_bouncy(self):
+        pass
+
+    def test_no_cardinality(self):
+        pass
+
+    def test_mine(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
