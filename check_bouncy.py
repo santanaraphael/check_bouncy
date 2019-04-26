e1cbf51405a0eb46e577960978611ee1b5a17790
Raphael Santana <uroboros.phael@gmail.com>
2019-04-26 20:30:55 -0300

    Adding check_bouncy function.

--- check_bouncy.py
+++ check_bouncy.py
@@ -1,9 +1,22 @@
 
 def check_bouncy(number):
-    pass
+    number = str(number)
+    increasing = 0
+    decreasing = 0
+
+    for index, digit in enumerate(number):
+        if index + 1 == len(number):
+            return not (increasing == len(number) - 1 or decreasing == len(number) - 1)
+        else:
+            if digit >= number[index + 1]:
+                increasing += 1
+            if digit <= number[index + 1]:
+                decreasing += 1
+
 
 def bouncy_number(target):
-    pass
+    return None
+
 
 def challenge():
-    pass
+    return None
