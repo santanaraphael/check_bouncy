ebbf762ac7de0b85f7fb7d501f4f053ec1c57b37
Raphael Santana <uroboros.phael@gmail.com>
2019-04-27 01:24:21 -0300

    Testing for bad arguments.

--- test_check_bouncy.py
+++ test_check_bouncy.py
@@ -2,7 +2,7 @@ import unittest
 import check_bouncy as cb
 
 
-class TestChecker(unittest.TestCase):
+class TestBouncyNumbers(unittest.TestCase):
 
     def test_check_bouncy(self):
         self.assertFalse(cb.check_bouncy(134468))
@@ -14,6 +14,13 @@ class TestChecker(unittest.TestCase):
         self.assertEqual(cb.bouncy_number(538), 538*0.5)
         self.assertEqual(cb.bouncy_number(21780), 21780*0.9)
 
+    def test_bad_args(self):
+        self.assertRaises(TypeError, cb.check_bouncy, 323.23)
+        self.assertRaises(TypeError, cb.bouncy_number, 323.23)
+        self.assertRaises(TypeError, cb.check_bouncy, 'hollywood')
+        self.assertRaises(TypeError, cb.bouncy_number, 'coffee')
+        self.assertRaises(TypeError, cb.challenge, 'foo')
+        self.assertRaises(ValueError, cb.challenge, 1)
 
 if __name__ == '__main__':
     unittest.main()
