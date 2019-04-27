b312a8fe03aa97c2a8c9b6f68fd6292efa121cee
Raphael Santana <uroboros.phael@gmail.com>
2019-04-26 21:37:06 -0300

    Adding challenge function and improving performance.

--- check_bouncy.py
+++ check_bouncy.py
@@ -1,17 +1,19 @@
 
 def check_bouncy(number):
     number = str(number)
-    increasing = 0
-    decreasing = 0
+    increasing = True
+    decreasing = True
 
     for index, digit in enumerate(number):
+        if not (increasing or decreasing):
+            return True
         if index + 1 == len(number):
-            return not (increasing == len(number) - 1 or decreasing == len(number) - 1)
+            return not (increasing or decreasing)
         else:
-            if digit >= number[index + 1]:
-                increasing += 1
-            if digit <= number[index + 1]:
-                decreasing += 1
+            if digit < number[index + 1]:
+                increasing = False
+            if digit > number[index + 1]:
+                decreasing = False
 
 
 def bouncy_number(target):
@@ -25,5 +27,17 @@ def bouncy_number(target):
 
 
 def challenge():
-    return None
+    number = 1
+    results = 0
+    while True:
+        if check_bouncy(number):
+            results += 1
+        if results / number >= 0.99:
+            return number
+        print(number, results, results/number*100)
+        number += 1
+
+print(challenge())
+
+
 
